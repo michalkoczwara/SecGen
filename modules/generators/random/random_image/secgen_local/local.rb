c7b7292ca3815f30e8ac811f220d25ee95238322
thomashaw <thomashaw@gmail.com>
2017-03-10 16:59:39 +0000

    Random image -- removed the file steps as they are unnecessary if we're passing image data around as base64

--- modules/generators/random/random_image/secgen_local/local.rb
+++ modules/generators/random/random_image/secgen_local/local.rb
@@ -12,28 +12,16 @@ class ImageGenerator < StringEncoder
   end
 
   def encode_all
-    # Get all Filenames in directory
     image_filenames = Dir["#{IMAGES_DIR}/*"]
-    # Select one
     selected_filename = image_filenames.sample
 
-    # Copy the file to a temporary location and operate on the temp file.
-    temp_file_path = File.expand_path('../../files/tmp.jpg', __FILE__)
-    FileUtils.cp(selected_filename, temp_file_path)
-
-    # Open temp file handle + append our string
-    File.open(temp_file_path, 'a') { |f| f.write(self.string_to_insert) }
-
-    contents = File.binread(temp_file_path)
-    encoded = Base64.encode64(contents)
-    # contents << File.readlines(temp_file_path)
-    self.outputs << encoded
+    file_contents = File.binread(selected_filename)
+    self.outputs << Base64.strict_encode64(file_contents)
   end
 
   def encoding_print_string
     'Random image generator'
   end
-
 end
 
 ImageGenerator.new.run
